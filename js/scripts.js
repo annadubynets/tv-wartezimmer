5e4ceda1fd615f2601b7524658fb7812eae4f2b3
Ivan Dubynets <ivan.dubynets@gmail.com>
2021-11-25 22:17:27 +0200

    skeleton tuning

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,53 @@
-if ($('.quick-access .owl-carousel').length > 0) {
-    $('.quick-access .owl-carousel').owlCarousel({
-        loop: true,
-        margin: 60,
-        responsiveClass: true,
-        autoplay: false,
-        autoWidth: true,
-        dots: false,
-        nav: false,
-        responsive: {
-            0: {
-                items: 2,
-                stagePadding: 60,
-            },
-            992: {
-                items: 4
-            }
-        }
-    })
-}
 
-if ($('.themed-packages .owl-carousel').length > 0) {
-    $('.themed-packages .owl-carousel').owlCarousel({
-        loop: true,
-        margin: 60,
-        responsiveClass: true,
-        autoplay: false,
-        autoWidth: true,
-        dots: false,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1,
-                stagePadding: 60,
-            },
-            992: {
-                items: 2
+// init carousels
+$(function() {
+    if ($('.quick-access .owl-carousel').length > 0) {
+        $('.quick-access .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 60,
+            responsiveClass: true,
+            autoplay: false,
+            autoWidth: true,
+            dots: false,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 2,
+                    stagePadding: 60,
+                },
+                992: {
+                    items: 4
+                }
             }
-        }
+        })
+    }
+    
+    if ($('.themed-packages .owl-carousel').length > 0) {
+        $('.themed-packages .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 60,
+            responsiveClass: true,
+            autoplay: false,
+            autoWidth: true,
+            dots: false,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    stagePadding: 60,
+                },
+                992: {
+                    items: 2
+                }
+            }
+        })
+    }
+});
+
+$(function() {
+    $('.sidebar-toggler').on('click', function() {
+        const sidebarSelector = $(this).data('target');
+        $(this).toggleClass('expanded');
+        $(sidebarSelector).toggleClass('show');
     })
-}
+});
